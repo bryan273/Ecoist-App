0b8bf17840c058d08b957828072e9c1e511337d8
azmirahmadisha <88032740+azmirahmadisha@users.noreply.github.com>
2022-12-10 23:01:12 +0700

    Update drawer_admin.dart

--- lib/landing/components/drawer_admin.dart
+++ lib/landing/components/drawer_admin.dart
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
-=======
 import 'package:ecoist/admin_ecoist/page/add_notes.dart';
->>>>>>> 2e1186a14465840c5491e58f75ffc14f6aca1f25
 import 'package:ecoist/landing/api/landing_api.dart';
 import 'package:ecoist/main.dart';
 import 'package:ecoist/admin_ecoist/page/admin_ecoist.dart';
 import 'package:ecoist/admin_ecoist/page/top_user_page.dart';
+import 'package:ecoist/admin_ecoist/component/form_add_notes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
@@ -19,20 +17,6 @@ class DrawerAdmin extends StatelessWidget {
     return Drawer(
       child: Column(
         children: [
-          // Menambahkan clickable menu
-          ListTile(
-            title: const Text('Home'),
-            onTap: () {
-              // Route menu ke halaman utama
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const MyHomePage(
-                      title: "Admin",
-                    )),
-              );
-            },
-          ),
           ListTile(
             title: const Text('Dashboard'),
             onTap: () {
@@ -52,6 +36,17 @@ class DrawerAdmin extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                     builder: (context) => const TopUserPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Add Notes'),
+            onTap: () {
+              // Route menu ke halaman utama
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const AddNotesPage()),
               );
             },
           ),
