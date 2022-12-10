480b64ba17fb2301eaed1581e18275dc093eb302
mazayanur11 <mazaya.nur11@ui.ac.id>
2022-12-10 20:42:02 +0700

    benerin modal

--- lib/landing/components/drawer_admin.dart
+++ lib/landing/components/drawer_admin.dart
@@ -1,3 +1,4 @@
+import 'package:ecoist/donate/page/donate.dart';
 import 'package:ecoist/landing/api/landing_api.dart';
 import 'package:ecoist/main.dart';
 import 'package:ecoist/admin_ecoist/page/admin_ecoist.dart';
