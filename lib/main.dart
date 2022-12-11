4bdb5d272920b99d70800b2f19faa7559f4453fd
Roy Maruli Tua Nababan <roy.maruli@ui.ac.id>
2022-12-11 21:33:32 +0700

    main page added image (edit pubspec.yaml and add assets)

--- lib/main.dart
+++ lib/main.dart
@@ -83,53 +83,38 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text("ECOIST"),
       ),
       drawer:
-      const DrawerUser(),
-      // (widget.title == "-") || (widget.title == "ECOIST")
-      // ? const DrawerUnlogin()
-      // : widget.title == "Admin"
-      //     ? const DrawerAdmin()
-      //     : const DrawerUser(),
+      //const DrawerUser(),
+        (widget.title == "-") || (widget.title == "ECOIST")
+        ? const DrawerUnlogin()
+        : widget.title == "Admin"
+          ? const DrawerAdmin()
+          : const DrawerUser(),
 
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Image.asset(
+              'assets/images/forest.jpg',
+              height: 300,
+              fit: BoxFit.fitWidth,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Welcome to Ecoist, where we can create a better world together.',
+            ),
+            const SizedBox(
+              height: 30,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
